1ad2f6ff9384df8c3cb7b7626a1bb6c1b8b5588c
Byron Tottaram <byron.tottaram@alumni.stonybrook.edu>
2020-02-09 18:42:29 -0500

    initial upload of files

---
+++ app/data/friends.js
@@ -0,0 +1,48 @@
+var friends = [
+    {
+        name: "Malcolm Reynolds",
+        photo: 
+        "https://i.imgur.com/EDEzHrx.jpg",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    },
+    {
+        name: "Jayne Cobb",
+        photo:
+        "https://i.imgur.com/0kwbBxR.gif",
+        scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+    },
+    {
+        name: "River Tam",
+        photo: 
+        "https://i.imgur.com/uxVREyk.gif",
+        scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+    },
+
+    {
+        name: "Hoban Washburne",
+        photo: 
+        "https://i.imgur.com/gpZoa8C.gif",
+        scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+    },
+
+    {
+        name: "Inara Serra",
+        photo:
+        "https://i.imgur.com/webs9Lk.jpg",
+        scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+    },
+
+    {
+        name: "Shepard Book",
+        photo:
+        "https://i.imgur.com/5cFiqvV.gif",
+        scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
+    },
+
+    {
+        name: "Zoe Washburne",
+        photo: "https://i.imgur.com/asK9GEg.jpg",
+        scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+    }
+];
+module.exports = friends;
