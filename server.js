fc76d791c51b38d2679e113bac236a899a67c009
Byron Tottaram <byron.tottaram@alumni.stonybrook.edu>
2020-02-09 19:56:55 -0500

    completed js files, and initial bootstrap layout but troubleshooting server.js file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
-var port = process.env.PORT || 8081;
+var port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,8 +12,7 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.use(express.static("app/public"));
 
-require("./app/routing/api-routes")(app);
-require("./app/routing/html-routes")(app);
-
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(port, () => console.log("Listening on port %s", port));
